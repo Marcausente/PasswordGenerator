e223ae3adcf28b1d56e12dc0926ae94f7d6b17dd
Marc Fernández <149372293+Marcausente@users.noreply.github.com>
2024-04-04 11:47:19 +0200

    cambios

--- src/contrasenagen.java
+++ src/contrasenagen.java
@@ -4,19 +4,16 @@ import java.util.Scanner;
 
 public class contrasenagen {
     private static Scanner input = new Scanner(System.in);
-    static int [] letras = new int [26];
-    static String[] nombres = {"Adrian", "Paco"};
+    static String[] letras = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
+    static String[] numeros = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
     public static void main(String[] args) {
         int digitos = 0;
         int menu = 0;
-        declararletras();
         printmenu();
-
     }
 
     private static void printmenu() {
+        System.out.println(letras[3]);
     }
 
-    private static void declararletras() {
-    }
 }
