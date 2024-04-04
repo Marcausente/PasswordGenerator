61ec58f32432b5e56d70f9bb278015897632c366
Marc Fernández <149372293+Marcausente@users.noreply.github.com>
2024-04-04 11:20:11 +0200

    First

---
+++ src/contrasenagen.java
@@ -0,0 +1,22 @@
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class contrasenagen {
+    private static Scanner input = new Scanner(System.in);
+    static int [] letras = new int [26];
+    static String[] nombres = {"Adrian", "Paco"};
+    public static void main(String[] args) {
+        int digitos = 0;
+        int menu = 0;
+        declararletras();
+        printmenu();
+
+    }
+
+    private static void printmenu() {
+    }
+
+    private static void declararletras() {
+    }
+}
